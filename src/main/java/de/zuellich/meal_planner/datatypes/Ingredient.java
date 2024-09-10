05dfb5bea29c6ce1b0180d899728e6ea576a0067
Erfan Arvan <ea442@njit.edu>
2024-09-09 23:57:20 -0400

    removed commands, annos, and imports

--- src/main/java/de/zuellich/meal_planner/datatypes/Ingredient.java
+++ src/main/java/de/zuellich/meal_planner/datatypes/Ingredient.java
@@ -1,34 +1,22 @@
 package de.zuellich.meal_planner.datatypes;
-
-import java.util.Objects;
-
-/** Represents an ingredient. */
 public class Ingredient {
-
   private IngredientUnit unit;
-
   private String type;
-
   private float amount;
-
   public Ingredient(String type, float amount, IngredientUnit unit) {
     this.type = type;
     this.amount = amount;
     this.unit = unit;
   }
-
   public IngredientUnit getUnit() {
     return unit;
   }
-
   public String getType() {
     return type;
   }
-
   public float getAmount() {
     return amount;
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -38,12 +26,10 @@ public class Ingredient {
         && getUnit() == that.getUnit()
         && Objects.equals(getType(), that.getType());
   }
-
   @Override
   public int hashCode() {
     return Objects.hash(getUnit(), getType(), getAmount());
   }
-
   @Override
   public String toString() {
     return "Ingredient{" + "unit=" + unit + ", type='" + type + '\'' + ", amount=" + amount + '}';
